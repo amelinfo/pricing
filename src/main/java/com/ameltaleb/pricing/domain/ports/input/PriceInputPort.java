f3f13c5c5f4672083496f5c1c1146530df6acee7
amelinfo <taleb.amelinfo@gmail.com>
2025-06-10 23:23:55 +0200

    feat: implementation of ports

--- src/main/java/com/ameltaleb/pricing/domain/ports/input/PriceInputPort.java
+++ src/main/java/com/ameltaleb/pricing/domain/ports/input/PriceInputPort.java
@@ -1,6 +1,5 @@
 package com.ameltaleb.pricing.domain.ports.input;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 import com.ameltaleb.pricing.domain.model.aggregate.Price;
@@ -8,7 +7,7 @@ import com.ameltaleb.pricing.domain.model.valueobject.BrandId;
 import com.ameltaleb.pricing.domain.model.valueobject.ProductId;
 
 public interface PriceInputPort {
-Optional<Price> findApplicablePrice(LocalDateTime applicationDate, 
+Optional<Price> findApplicablePrice(String applicationDate, 
                                       ProductId productId, 
                                       BrandId brandId);
 }
--- src/main/java/com/ameltaleb/pricing/domain/ports/output/PriceOutputPort.java
+++ src/main/java/com/ameltaleb/pricing/domain/ports/output/PriceOutputPort.java
@@ -7,5 +7,7 @@ import com.ameltaleb.pricing.domain.model.valueobject.BrandId;
 import com.ameltaleb.pricing.domain.model.valueobject.ProductId;
 
 public interface PriceOutputPort {
-     List<Price> findPricesByProductAndBrand(ProductId productId, BrandId brandId);
+     List<Price> findPricesByProductAndBrand(BrandId brandId, ProductId productId);
+
+     List<Price> findByBrandIdAndProductIdAndDate(BrandId brandId, ProductId productId, String applicationDate);
 }
